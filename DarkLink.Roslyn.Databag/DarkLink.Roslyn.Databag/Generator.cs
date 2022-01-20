c7d1807c5d5bd81c8cbfd112e92cf92d8b45d6ef
Waldemar Tomme <waldetomme@gmail.com>
2022-01-20 12:13:49 +0100

    Add project files.

---
+++ DarkLink.Roslyn.Databag/DarkLink.Roslyn.Databag/Generator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+namespace DarkLink.Roslyn.Databag
+{
+    [Generator]
+    public class Generator : ISourceGenerator, ISyntaxReceiver
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // Generate source code
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => this);
+
+            // Initialize generator
+        }
+
+        public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+        {
+            // Collect relevant syntax nodes
+        }
+    }
+}
